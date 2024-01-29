8d3eb71c19545794aed617054cd372416f88ee6a
Vasco Santos <santos.vasco10@gmail.com>
2024-01-29 19:13:02 +0100

    fix: make it work with dynamic import

--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,16 @@
-// @ts-expect-error no type declarations
-import bytecode from '../gen/wasm_bg.wasm'
-
 import load, { create } from "../gen/wasm.js"
 import { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE } from './constant.js'
 export * from "./type.js"
 export { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE }
+
+let bytecode
+
+/* c8 ignore start */
+try {
+  // @ts-expect-error no declaration types
+  bytecode = (await import('../gen/wasm_bg.wasm')).default
+} catch {}
+/* c8 ignore stop */
 
 await load(bytecode)
 
