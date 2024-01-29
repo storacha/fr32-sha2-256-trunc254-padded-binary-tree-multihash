d1810e0d5e53909500dc7290db4e8dcc6f8e72f8
Vasco Santos <santos.vasco10@gmail.com>
2024-01-29 10:02:39 +0100

    fix: address review comments

--- gen/wasm.js
+++ gen/wasm.js
@@ -256,13 +256,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        try {
-            input = new URL('wasm_bg.wasm', import.meta.url);
-        } catch {
-            // try with dynamic import - CF workers runtime does not have import.meta.url
-            const module = await import('./wasm_bg.wasm')
-            input = module.default
-        }
+        input = new URL('wasm_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,11 @@
+// @ts-expect-error no type declarations
+import bytecode from '../gen/wasm_bg.wasm'
+
 import load, { create } from "../gen/wasm.js"
 import { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE } from './constant.js'
 export * from "./type.js"
 export { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE }
 
-await load()
+await load(bytecode)
 
 export { create }
