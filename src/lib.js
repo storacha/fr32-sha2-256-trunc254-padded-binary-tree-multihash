d8c985f565dabcb5439346900dc0587e0a58ddb9
Vasco Santos <santos.vasco10@gmail.com>
2024-01-29 19:22:55 +0100

    fix: check cf specific

--- src/lib.js
+++ src/lib.js
@@ -3,12 +3,16 @@ import { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE } from './constant.
 export * from "./type.js"
 export { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE }
 
+// load bytecode in Cloudflare Workers as wasm import
+// all other paths are disallowed by embedder
 let bytecode
-
 /* c8 ignore start */
 try {
-  // @ts-expect-error no declaration types
-  bytecode = (await import('../gen/wasm_bg.wasm')).default
+  // @ts-expect-error only in Cloudflare
+  if (HTMLRewriter) {
+    // @ts-expect-error no declaration types
+    bytecode = (await import('../gen/wasm_bg.wasm')).default
+  }
 } catch {}
 /* c8 ignore stop */
 
