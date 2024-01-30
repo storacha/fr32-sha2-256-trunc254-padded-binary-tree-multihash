dc15e37587112dcd10455c7791a5384fc8e32151
Vasco Santos <santos.vasco10@gmail.com>
2024-01-30 16:18:19 +0100

    fix: load bytecode if possible to support Cloudflare Workers as wasm import (#31)

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,30 @@
 import load, { create } from "../gen/wasm.js"
-import { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE } from './constant.js'
+import {
+  code,
+  CODE_LENGTH,
+  HEIGHT_SIZE,
+  ROOT_SIZE,
+  MAX_SIZE,
+} from "./constant.js"
 export * from "./type.js"
 export { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE }
 
-await load()
+// load bytecode in Cloudflare Workers as wasm import
+// all other paths are disallowed by embedder
+let bytecode
+// https://developers.cloudflare.com/workers/runtime-apis/web-standards/#navigatoruseragent
+export const CLOUDFLARE_WORKERS_NAVIGATOR = "Cloudflare-Workers"
+/* c8 ignore start */
+try {
+  if (globalThis.navigator?.userAgent === CLOUDFLARE_WORKERS_NAVIGATOR) {
+    // playwright tries to bundle imported file when it sees import with
+    // string literal. We workaround by using variable to import.
+    const wasm = "../gen/wasm_bg.wasm"
+    bytecode = (await import(wasm)).default
+  }
+} catch {}
+/* c8 ignore stop */
+
+await load(bytecode)
 
 export { create }
