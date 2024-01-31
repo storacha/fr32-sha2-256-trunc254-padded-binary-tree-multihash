cc73afabd5f2a1837af40ed529b2d32e78cf7cc3
Vasco Santos <santos.vasco10@gmail.com>
2024-01-31 10:24:00 +0100

    feat: export cf compatible module (#33)
    Based on issues still rising on a Cloudflare deployment with
    https://github.com/web3-storage/fr32-sha2-256-trunc254-padded-binary-tree-multihash/pull/31,
    we decided to just make a new export for this purpose.
    
    Also added Miniflare tests injecting a worker script that uses the
    exported cloudflare module. In a next PR I intend to add docs on how to
    make this work with a bundler to replace one for the other (or maybe
    will be in w3up-client, still tbd)

--- src/wasm-import.js
+++ src/wasm-import.js
@@ -9,6 +9,12 @@ import {
 export * from "./type.js"
 export { code, CODE_LENGTH, HEIGHT_SIZE, ROOT_SIZE, MAX_SIZE }
 
-await load()
+// load bytecode with import
+// there are runtimes like cloudflare workers where
+// all other paths are disallowed by embedder
+// @ts-expect-error
+import bytecode from "../gen/wasm_bg.wasm"
+
+await load(bytecode)
 
 export { create }
